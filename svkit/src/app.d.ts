9aa09ead5f31bc9b7f8653ef968104ae4c775c67
Joakim Olsson <barbatron@gmail.com>
2024-10-31 20:56:13 +0100

    add account to PageData

--- svkit/src/app.d.ts
+++ svkit/src/app.d.ts
@@ -1,10 +1,14 @@
+import type { Account } from '$lib/appwrite';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			account?: Account;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
