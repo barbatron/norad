baabda6a3d64c4df7bfd4dafcd1188de4e9a64ea
Joakim Olsson <barbatron@gmail.com>
2024-10-31 21:57:13 +0100

    move maps api load to pagedata

--- svkit/src/lib/db.ts
+++ svkit/src/lib/db.ts
@@ -1,30 +1,30 @@
-import { PUBLIC_APPWRITE_COLL_ID, PUBLIC_APPWRITE_DB_ID } from "$env/static/public";
-import { Databases, ID } from "appwrite";
-import { client } from "./appwrite";
-import { hoggarn as hoggarnGeoJson } from './maps';
-import { logger } from "./observability";
+import { PUBLIC_APPWRITE_MAPS_COLL_ID, PUBLIC_APPWRITE_DB_ID } from '$env/static/public';
+import { Databases, ID } from 'appwrite';
+import { client } from './appwrite';
+import { logger } from './observability';
+import { hoggarnGeoJson } from './maps';
 
 const console = logger('lib db');
 
 const databases = new Databases(client);
 
 export const testCreateDoc = async () => {
-  const response = await databases.createDocument(
-    PUBLIC_APPWRITE_DB_ID,
-    PUBLIC_APPWRITE_COLL_ID,
-    ID.unique(),
-    { "name": "test hoggarn", "geoJson": JSON.stringify(hoggarnGeoJson) }
-  );
-  console.log({ response })
-}
+	const response = await databases.createDocument(
+		PUBLIC_APPWRITE_DB_ID,
+		PUBLIC_APPWRITE_MAPS_COLL_ID,
+		ID.unique(),
+		{ name: 'test hoggarn', geoJson: JSON.stringify(hoggarnGeoJson) }
+	);
+	console.log({ response });
+};
 
 export const loadMap = async (name = 'test hoggarn') => {
-  const { documents } = await databases.listDocuments(
-    PUBLIC_APPWRITE_DB_ID,
-    PUBLIC_APPWRITE_COLL_ID,
-  );
-  console.log('loadMap documents', documents)
-  const map = documents.find(map => map.name == name);
-  console.log('loadMap map', map)
-  return map?.geoJson;
-}
+	const { documents } = await databases.listDocuments(
+		PUBLIC_APPWRITE_DB_ID,
+		PUBLIC_APPWRITE_MAPS_COLL_ID
+	);
+	console.log('loadMap documents', documents);
+	const map = documents.find((map) => map.name == name);
+	console.log('loadMap map', map);
+	return map?.geoJson;
+};
